4d725bcf73e23ac0600e6088d556019344b00b48
accurateguy0 <ykagui678@gmail.com>
2025-06-21 09:57:17 +0200

    Update index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@ require __DIR__ . '/vendor/autoload.php';
 use Cowsayphp\Farm;
 
 header('Content-Type: text/plain');
-
+?message="stringconcat";
 $text = "Set a message by adding ?message=<message here> to the URL";
 if(isset($_GET['message']) && $_GET['message'] != '') {
 	$text = htmlspecialchars($_GET['message']);
